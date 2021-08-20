63c8b25c96ada357e6e04d17e7c34c0bab568490
Michael Kovacevich <mkovacevich@Michaels-Mac-mini.local>
2021-08-20 16:54:09 -0700

    extended the README and added two array equivalency functions

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# nucleus
+# Nucleus
+
 A collection of essential utilities designed to be the central and most important part of any Typescript Developer's toolkit.
+
+## Getting Started
+
+### Install
+
+```shell
+npm install --save @behaveware/nucleus
+```
+
+### Documentation
+
+Documentation is currently inside the code. All exported functions have a comment included that will show up in your text editor explaining the function's purpose as well as its parameters and return value. External documentation will be coming in the future, reach out to the maintainers if you are interested in contributing external documentation.
+
+## Getting Started for Contributors
+
+- Pull down repository:
+
+  ```shell
+  git clone https://github.com/BehaveWare/nucleus.git
+  ```
+
+- Run all unit tests:
+
+  ```shell
+  npm test
+  ```
+
+- Run all tests with coverage:
+
+  ```shell
+  npm run coverage
+  ```
+
+- Run the linter:
+
+  ```shell
+  npm run lint
+  ```
--- src/arrays.ts
+++ src/arrays.ts
@@ -1,3 +1,5 @@
+import { IComparable } from "./types";
+
 /**
  * Breaks an array T[] into an array of batches T[][]
  * @param array Array to break into batches
@@ -24,4 +26,40 @@ export const batchArray = <T>(
   }
 
   return batches;
+};
+
+/**
+ * Compares two arrays of type T where T extends IComparable
+ * @param a Array of IComparable objects
+ * @param b Array of IComparable objects
+ * @returns boolean
+ */
+export const compareArrays = <T extends IComparable<T>>(
+  a: T[],
+  b: T[],
+) => {
+  return (
+    a.every((x) => b.some((y) => x.equals(y))) &&
+    b.every((x) => a.some((y) => x.equals(y))) &&
+    a.length === b.length
+  );
+};
+
+/**
+ * Determines if two arrays of type T[] are equal
+ * @param a Array of objects
+ * @param b Array of objects
+ * @param equals Function for comparing objects of type T
+ * @returns boolean
+ */
+export const arraysAreEqual = <T>(
+  a: T[],
+  b: T[],
+  equals: (a: T, b: T) => boolean,
+) => {
+  return (
+    a.every((x) => b.some((y) => equals(x, y))) &&
+    b.every((x) => a.some((y) => equals(x, y))) &&
+    a.length === b.length
+  );
 };
