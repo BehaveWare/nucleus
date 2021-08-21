e389f81fa2b936468e6d410fedc1359797747cef
Michael Kovacevich <mkovacevich@Michaels-Mac-mini.local>
2021-08-20 19:25:20 -0700

    playing around with generic parameter names

--- src/generics.ts
+++ src/generics.ts
@@ -1,8 +1,8 @@
 /**
- * Determines if two items of type T are equivalent when stringified
- * @param one First thing to compare
- * @param another Second thing to compare
+ * Determines if two objects of type T are equivalent when stringified
+ * @param a First object to compare
+ * @param b Second object to compare
  * @returns A boolean representing whether the two are equal as strings
  */
-export const equalAsStrings = <T>(one: T, another: T) =>
-  JSON.stringify(one) === JSON.stringify(another);
+export const equalAsStrings = <T>(a: T, b: T) =>
+  JSON.stringify(a) === JSON.stringify(b);
